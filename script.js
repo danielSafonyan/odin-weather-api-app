f527cd029e606ac55cc197d067fb63085a2dec65
danielSafonyan <danielsafonyan@gmail.com>
2022-09-26 18:23:56 +0100

    Implemented Weather API

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("I am here!")
+import view from './view.js';
+
+const apiKey = 'cbe4068c303885baf947474faa0052c1';
+let city = 'Moscow'
+let units = 'metric';
+let gif = '';
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cbe4068c303885baf947474faa0052c1&units=${units}`
+
+async function getWeather() {
+    const response = await fetch(url, {'mode': 'cors'});
+    const responseData = await response.json();
+    console.log(responseData);
+    view.updateWeather(responseData);
+}
+
+getWeather();
---
+++ view.js
@@ -0,0 +1,35 @@
+function getLocalTime(epochTime, offset) {
+    console.log(offset);
+    const date = new Date((epochTime + offset) * 1000);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}:${minutes}`
+}
+
+class View {
+    constructor() {
+        this.location = document.querySelector('#location');
+        this.temp = document.querySelector('#temp');
+        this.minTemp = document.querySelector('#min-temp');
+        this.maxTemp = document.querySelector('#max-temp');
+        this.weatherDescr = document.querySelector('#weather-descr');
+        this.sunrise = document.querySelector('#sunrise');
+        this.sunset = document.querySelector('#sunset');
+        this.humidity = document.querySelector('#humidity');
+        this.pressure = document.querySelector('#pressure');
+    }
+    updateWeather(weatherObject) {
+        this.location.innerText = weatherObject.name;
+        this.temp.innerText = weatherObject.main.temp;
+        this.minTemp.innerText = weatherObject.main.temp_min;
+        this.maxTemp.innerText = weatherObject.main.temp_max;
+        this.weatherDescr.innerText = weatherObject.weather[0].description;
+        this.sunrise.innerText = getLocalTime(weatherObject.sys.sunrise, weatherObject.timezone);
+        this.sunset.innerText = getLocalTime(weatherObject.sys.sunset, weatherObject.timezone);
+        this.humidity.innerText = weatherObject.main.humidity; 
+        this.pressure.innerText = weatherObject.main.pressure;
+    }
+}
+
+const view = new View();
+export default view;
